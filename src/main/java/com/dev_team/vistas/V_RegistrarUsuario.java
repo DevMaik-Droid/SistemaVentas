8448941d415583c87c608e9037ba680fa6b31e3d
DevMaik <DevMaik@DESKTOP-D5PGKSO>
2024-04-03 20:39:52 -0400

    Cambios en el dashboad

--- src/main/java/com/dev_team/vistas/V_RegistrarUsuario.java
+++ src/main/java/com/dev_team/vistas/V_RegistrarUsuario.java
@@ -20,6 +20,7 @@ public class V_RegistrarUsuario extends Gradient {
     public V_RegistrarUsuario() {
         super(Main_Colores.C_600,Main_Colores.C_500,Main_Colores.C_400);
         initComponents();
+        
     }
 
     
@@ -35,6 +36,7 @@ public class V_RegistrarUsuario extends Gradient {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jTextField2 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -43,44 +45,163 @@ public class V_RegistrarUsuario extends Gradient {
         jTextField6 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jTextField7 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField8 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField9 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jTextField10 = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        btn_registrar = new javax.swing.JButton();
+        jLabel10 = new javax.swing.JLabel();
+        btn_buscar = new javax.swing.JButton();
+        jLabel11 = new javax.swing.JLabel();
+        jPasswordField2 = new javax.swing.JPasswordField();
+        jLabel12 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(51, 51, 51));
-        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(76, 94, 310, 34));
+        setLayout(new java.awt.BorderLayout());
+
+        jPanel1.setOpaque(false);
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jTextField2.setFont(new java.awt.Font("Righteous", 0, 14)); // NOI18N
+        jTextField2.setBorder(null);
+        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 100, 310, 34));
 
         jLabel1.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
         jLabel1.setForeground(Main_Colores.C_100);
         jLabel1.setText("Nombre:");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(76, 68, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 70, -1, -1));
 
         jLabel2.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
         jLabel2.setForeground(Main_Colores.C_100);
-        jLabel2.setText("Apellido:");
-        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 70, -1, 20));
-        add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 90, 300, 34));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Foto");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 70, 190, 20));
+
+        jTextField5.setBorder(null);
+        jPanel1.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 100, 300, 34));
 
         jLabel3.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
         jLabel3.setForeground(Main_Colores.C_100);
-        jLabel3.setText("Nombre:");
-        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 170, -1, -1));
-        add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 200, 283, 34));
+        jLabel3.setText("Fecha naciemiento:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 160, -1, -1));
+
+        jTextField6.setBorder(null);
+        jPanel1.add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 190, 310, 34));
 
         jLabel4.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
         jLabel4.setForeground(Main_Colores.C_100);
-        jLabel4.setText("Nombre:");
-        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 350, -1, -1));
-        add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 380, 283, 34));
+        jLabel4.setText("Cedula Identidad:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 160, -1, -1));
+
+        jTextField7.setBorder(null);
+        jPanel1.add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 190, 300, 34));
+
+        jLabel5.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel5.setForeground(Main_Colores.C_100);
+        jLabel5.setText("Telefono");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, -1, -1));
+
+        jTextField8.setBorder(null);
+        jPanel1.add(jTextField8, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 270, 310, 34));
+
+        jLabel6.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel6.setForeground(Main_Colores.C_100);
+        jLabel6.setText("Observaciones: ");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 330, -1, -1));
+
+        jTextField9.setBorder(null);
+        jPanel1.add(jTextField9, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 270, 300, 34));
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jTextArea1.setBorder(null);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 350, 650, 100));
+
+        jLabel7.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel7.setForeground(Main_Colores.C_100);
+        jLabel7.setText("Direccion:");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 240, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel8.setForeground(Main_Colores.C_100);
+        jLabel8.setText("Usuario:");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 480, -1, -1));
+
+        jTextField10.setBorder(null);
+        jPanel1.add(jTextField10, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 510, 260, 34));
+
+        jLabel9.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel9.setForeground(Main_Colores.C_100);
+        jLabel9.setText("Contraseña:");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 480, -1, -1));
+
+        jPasswordField1.setText("jPasswordField1");
+        jPasswordField1.setBorder(null);
+        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 510, 180, 30));
+
+        btn_registrar.setText("REGISTRAR");
+        jPanel1.add(btn_registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 580, 190, 50));
+
+        jLabel10.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel10.setOpaque(true);
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 100, 191, 196));
+
+        btn_buscar.setText("Buscar");
+        jPanel1.add(btn_buscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 320, 130, -1));
+
+        jLabel11.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel11.setForeground(Main_Colores.C_100);
+        jLabel11.setText("Apellido:");
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 70, -1, 20));
+
+        jPasswordField2.setText("jPasswordField1");
+        jPasswordField2.setBorder(null);
+        jPanel1.add(jPasswordField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 510, 180, 30));
+
+        jLabel12.setFont(new java.awt.Font("Lucida Console", 0, 18)); // NOI18N
+        jLabel12.setForeground(Main_Colores.C_100);
+        jLabel12.setText("Confirmar:");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 480, -1, -1));
+
+        add(jPanel1, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_buscar;
+    private javax.swing.JButton btn_registrar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JPasswordField jPasswordField2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField10;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField5;
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
+    private javax.swing.JTextField jTextField8;
+    private javax.swing.JTextField jTextField9;
     // End of variables declaration//GEN-END:variables
 }
